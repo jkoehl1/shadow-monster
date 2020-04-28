828ee0c2d1fefebe8c8fc9c9b50220ca9b1b75ba
jkoehl1 <64221787+jkoehl1@users.noreply.github.com>
2020-04-27 22:31:04 -0400

    Final code

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function backHome () {
-  window.location = 'page15.html'
-  let video = document.querySelector('videos/ShadowPt13.mp4')
-  video.addEventListener('ended', backHome )
+window.location = 'page15.html'
 }
+
+let video = document.querySelector('videos/ShadowPt13.mp4')
+video.addEventListener('ended', backHome )
